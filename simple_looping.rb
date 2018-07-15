c576e443f0a56d9aaae51887988ecf6105f010a9
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-15 01:30:32 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,20 +1,41 @@
-# REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(number_of_times)
+
+def times_iterator(number_of_times)
+  
+  number_of_times.times do
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
+
+end
+
+def while_iterator(number_of_times)
   
   counter = 0 
   
-  loop do 
-    
+  while counter < number_of_times
     puts "Welcome to Flatiron School's Web Development Course!"
-    counter++ 
-    
-    if counter >= number_of_times
-      break
-    end
-    
   end
   
 end
 
+def until_iterator(number_of_times)
+   
+   counter = 0 
+   
+   until counter == number_of_times
+      puts "Welcome to Flatiron School's Web Development Course!"
+    end
+
+end
+
+def for_iterator(number_of_times)
+  
+  counter = 1..number_of_times
+  
+  for count in counter 
+  
+    puts "Welcome to Flatiron School's Web Development Course!"
+    end
+  
+end
 
