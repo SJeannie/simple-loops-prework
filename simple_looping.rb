456fb13a16edbbfe743e8ab92bd3e0cb661534d2
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-15 01:29:45 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,7 +9,7 @@ def loop_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
     counter++ 
     
-    if counter == number_of_times
+    if counter >= number_of_times
       break
     end
     
