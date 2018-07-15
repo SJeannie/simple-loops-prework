6f5da031a30e4ec3dcbf44d65ac63a1db54ecf64
Jeannie Kim <sj.jeannie.kim@gmail.com>
2018-07-15 01:30:17 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -17,42 +17,4 @@ def loop_iterator(number_of_times)
   
 end
 
-def times_iterator(number_of_times)
-  
-  number_of_times.times do
-    puts "Welcome to Flatiron School's Web Development Course!"
-  end
-
-end
-
-def while_iterator(number_of_times)
-  
-  counter = 0 
-  
-  while counter < number_of_times
-    puts "Welcome to Flatiron School's Web Development Course!"
-  end
-  
-end
-
-def until_iterator(number_of_times)
-   
-   counter = 0 
-   
-   until counter == number_of_times
-      puts "Welcome to Flatiron School's Web Development Course!"
-    end
-
-end
-
-def for_iterator(number_of_times)
-  
-  counter = 1..number_of_times
-  
-  for count in counter 
-  
-    puts "Welcome to Flatiron School's Web Development Course!"
-    end
-  
-end
 
